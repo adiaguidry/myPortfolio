b2b0de15145915c2a60f9192c49db141828ff416
adiaguidry <adiaguidry@gmail.com>
2016-01-28 12:29:15 -0800

    update skills

--- js/mooz.themes.scripts.js
+++ js/mooz.themes.scripts.js
@@ -136,11 +136,3 @@ $(function() {
 });
     });
 
-//skills
-jQuery(document).ready(function(){
-    jQuery('.skillbar').each(function(){
-        jQuery(this).find('.skillbar-bar').animate({
-            width:jQuery(this).attr('data-percent')
-        },3000);
-    });
-});
